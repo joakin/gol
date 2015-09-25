248d22b47f7afd66bfaecdb0c0352b4d6ee3d283
Joaquin Oltra <joaquin@chimeces.com>
2015-09-25 14:43:15 -0600

    Cache dimensions and margins.
    And consistently generate initial board based on dimension

--- index.js
+++ index.js
@@ -9,10 +9,20 @@ var canvas = context.canvas
 document.body.appendChild(canvas)
 
 // setup a retina-scaled canvas
-var app = createLoop(canvas, { scale: window.devicePixelRatio })
+var app = window.app = createLoop(canvas, { scale: window.devicePixelRatio })
 
-app.board = initial(300)
+function update (app) {
+  app.width = app.shape[0] * app.scale
+  app.height = app.shape[1] * app.scale
+  var smallDim = Math.min(app.width, app.height)
+  app.cellSize = smallDim / app.board.length
+  app.left = (app.width - smallDim) / 2
+  app.top = (app.height - smallDim) / 2
+}
 
+app.board = initial(Math.round(Math.min(...app.shape)*app.scale/10))
+
+update(app)
 // start rendering
 app.start()
 
@@ -23,13 +33,11 @@ app.on('tick', function(dt) {
 
 // handle window resize
 app.on('resize', function() {
-  // the unscaled size
-  var width = app.shape[0]
-  var height = app.shape[1]
-
-  console.log('new canvas size', width, height)
+  update(app)
+  console.log('new canvas size', app.width, app.height)
 })
 
 canvas.addEventListener('click', function() {
   app.board = initial()
+  update(app)
 })
